362d171cbb5f20b75d395918ee5ec5465bc54558
Ulysse Herbach <35532736+ulysseherbach@users.noreply.github.com>
2026-01-27 22:24:24 +0100

    Update __init__.py

--- src/fluffyclone/__init__.py
+++ src/fluffyclone/__init__.py
@@ -1,14 +1,11 @@
 """
-FluffyClone
-===========
-
-Handling the weighted uniform distribution over spanning trees
+Handling the weighted uniform distribution over spanning trees.
 
 Weighted uniform distribution
 -----------------------------
 
 Reference: R. Lyons and Y. Peres (2021),
-Probability on trees and networks, Cambridge University Press, New York, NY.
+Probability on trees and networks, Cambridge University Press, New York.
 
 Random trees
 ------------
@@ -20,9 +17,9 @@ See https://github.com/ulysseherbach/fluffyclone for documentation.
 """
 from importlib.metadata import version as _version
 from fluffyclone.sampling import random_tree
-from fluffyclone.utils import distribution_estim
+from fluffyclone.utils import distribution_exact, distribution_estim
 
-__all__ = ["random_tree", "distribution_estim"]
+__all__ = ['distribution_estim', 'distribution_exact', 'random_tree']
 
 try:
     __version__ = _version('fluffyclone')
